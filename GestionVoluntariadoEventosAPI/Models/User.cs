c599e53eb7800c6618dd95da7f4819be947c95d1
Vladimir Cortés A. <vladimir.cortes@outlook.com>
2025-07-13 10:07:11 -0500

    feat(backend): refactor clase User. Adicione scaffolding.

--- GestionVoluntariadoEventosAPI/Models/User.cs
+++ GestionVoluntariadoEventosAPI/Models/User.cs
@@ -11,20 +11,27 @@ public partial class User
     [Key]
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre completo es obligatorio.")]
     [StringLength(100)]
     public string FullName { get; set; } = null!;
 
+    [Required(ErrorMessage = "El número de teléfono es obligatorio.")]
+    //[RegularExpression(@"^\+57[3][0-9]{9}$", ErrorMessage = "El número debe tener el formato +57XXXXXXXXXX.")]
     [StringLength(13)]
     public string PhoneNumber { get; set; } = null!;
 
+    [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido.")]
     [StringLength(100)]
     public string Email { get; set; } = null!;
 
+    [Required(ErrorMessage = "El nombre de usuario es obligatorio.")]
     [StringLength(50)]
     public string UserName { get; set; } = null!;
 
+    [Required(ErrorMessage = "La contraseña es obligatoria.")]
     [StringLength(100)]
+    //[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,100}$", ErrorMessage = "La contraseña debe tener al menos una letra mayúscula, una minúscula, un número y un carácter especial.")]
     public string Password { get; set; } = null!;
-
     public bool TermsAccepted { get; set; }
 }
